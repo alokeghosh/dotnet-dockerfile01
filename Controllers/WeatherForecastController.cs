4c365a4b4181c3534276818c319110f8b89ec839
Aloke <47846743+alokeghosh@users.noreply.github.com>
2025-12-23 12:54:25 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace samplewebapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "aloke", "NG", "Chilly", "Embee-dutta", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "aloke", "Sir", "Chilly", "Embee-dutta", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
